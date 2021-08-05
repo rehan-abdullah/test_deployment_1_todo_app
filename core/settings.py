535bc115b86727696d4e3418de0ed544e2059105
rehan-abdullah <31924510+rehan-abdullah@users.noreply.github.com>
2021-08-05 19:15:57 +0100

    Elephantsql

--- core/settings.py
+++ core/settings.py
@@ -80,22 +80,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https: // docs.djangoproject.com/en/3.2/ref/settings/  # databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'umdxaqvi',
-#         'USER': 'umdxaqvi',
-#         'PASSWORD': 'aECZpr2OcMaPSYIOvrt9jW1uCdOjgppv',
-#         'HOST': 'chunee.db.elephantsql.com',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'umdxaqvi',
+        'USER': 'umdxaqvi',
+        'PASSWORD': 'aECZpr2OcMaPSYIOvrt9jW1uCdOjgppv',
+        'HOST': 'chunee.db.elephantsql.com',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
