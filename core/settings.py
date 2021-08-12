3fbde3db88f191636a4910a9b909bf874dc27384
rehan-abdullah <31924510+rehan-abdullah@users.noreply.github.com>
2021-08-12 19:13:58 +0100

    Reconfigured static files.

--- core/settings.py
+++ core/settings.py
@@ -147,10 +147,10 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # STATIC FILES
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    "staticfiles",
+    "static",
 ]
 
 # Django REST settings
